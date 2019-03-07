0e35a3452a9d69be430a3bd434bd9c63604a0981
martinp95 <martinpelaezdiaz@gmail.com>
2019-03-07 13:57:17 +0100

    Struct ya definido identificacion

--- src/semantic/Identification.java
+++ src/semantic/Identification.java
@@ -8,9 +8,9 @@ import visitor.*;
 
 public class Identification extends DefaultVisitor {
 
-    public Identification(ErrorManager errorManager) {
-        this.errorManager = errorManager;
-    }
+	public Identification(ErrorManager errorManager) {
+		this.errorManager = errorManager;
+	}
 
 //	class Programa { List<Definicion> definicion; }
 	public Object visit(Programa node, Object param) {
@@ -24,7 +24,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class DefVariable { String nombre;  Tipo tipo;  String ambito; }
+	// class DefVariable { String nombre; Tipo tipo; String ambito; }
 	public Object visit(DefVariable node, Object param) {
 
 		// super.visit(node, param);
@@ -35,10 +35,12 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class DefStruct { String nombre;  List<Campo> campo; }
+	// class DefStruct { String nombre; List<Campo> campo; }
 	public Object visit(DefStruct node, Object param) {
 
 		// super.visit(node, param);
+		predicado(structs.get(node.getNombre()) == null, "Struct ya definido: " + node.getNombre(), node);
+		structs.put(node.getNombre(), node);
 
 		if (node.getCampo() != null)
 			for (Campo child : node.getCampo())
@@ -47,12 +49,13 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class DefFuncion { String nombre;  List<Parametro> parametro;  Tipo tipo;  List<DefVariable> defvariable;  List<Sentencia> sentencia; }
+	// class DefFuncion { String nombre; List<Parametro> parametro; Tipo tipo;
+	// List<DefVariable> defvariable; List<Sentencia> sentencia; }
 	public Object visit(DefFuncion node, Object param) {
 
 		// super.visit(node, param);
-		
-		predicado(funciones.get(node.getNombre())==null, "Funcion ya definida", node);
+
+		predicado(funciones.get(node.getNombre()) == null, "Funcion ya definida: " + node.getNombre(), node);
 		funciones.put(node.getNombre(), node);
 
 		if (node.getParametro() != null)
@@ -73,27 +76,27 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class IntType {  }
+	// class IntType { }
 	public Object visit(IntType node, Object param) {
 		return null;
 	}
 
-	//	class FloatType {  }
+	// class FloatType { }
 	public Object visit(FloatType node, Object param) {
 		return null;
 	}
 
-	//	class CharType {  }
+	// class CharType { }
 	public Object visit(CharType node, Object param) {
 		return null;
 	}
 
-	//	class IdentType { String nombre; }
+	// class IdentType { String nombre; }
 	public Object visit(IdentType node, Object param) {
 		return null;
 	}
 
-	//	class ArrayType { String posicion;  Tipo tipo; }
+	// class ArrayType { String posicion; Tipo tipo; }
 	public Object visit(ArrayType node, Object param) {
 
 		// super.visit(node, param);
@@ -104,7 +107,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Campo { String nombre;  Tipo tipo; }
+	// class Campo { String nombre; Tipo tipo; }
 	public Object visit(Campo node, Object param) {
 
 		// super.visit(node, param);
@@ -115,7 +118,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Parametro { String nombre;  Tipo tipo; }
+	// class Parametro { String nombre; Tipo tipo; }
 	public Object visit(Parametro node, Object param) {
 
 		// super.visit(node, param);
@@ -126,7 +129,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Print { Expr imprime;  String tipoPrint; }
+	// class Print { Expr imprime; String tipoPrint; }
 	public Object visit(Print node, Object param) {
 
 		// super.visit(node, param);
@@ -137,7 +140,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Read { Expr leer; }
+	// class Read { Expr leer; }
 	public Object visit(Read node, Object param) {
 
 		// super.visit(node, param);
@@ -148,7 +151,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Asignacion { Expr left;  Expr right; }
+	// class Asignacion { Expr left; Expr right; }
 	public Object visit(Asignacion node, Object param) {
 
 		// super.visit(node, param);
@@ -162,7 +165,8 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class If { Expr condicion;  List<Sentencia> verdadero;  List<Sentencia> falso; }
+	// class If { Expr condicion; List<Sentencia> verdadero; List<Sentencia> falso;
+	// }
 	public Object visit(If node, Object param) {
 
 		// super.visit(node, param);
@@ -181,7 +185,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class While { Expr condicion;  List<Sentencia> sentencia; }
+	// class While { Expr condicion; List<Sentencia> sentencia; }
 	public Object visit(While node, Object param) {
 
 		// super.visit(node, param);
@@ -196,11 +200,11 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class LlamadaFuncion { String nombre;  List<Expr> parametrosOpt; }
+	// class LlamadaFuncion { String nombre; List<Expr> parametrosOpt; }
 	public Object visit(LlamadaFuncion node, Object param) {
 
 		// super.visit(node, param);
-		predicado(funciones.get(node.getNombre())!=null, "Procedimiento no definido", node);
+		predicado(funciones.get(node.getNombre()) != null, "Procedimiento no definido: " + node.getNombre(), node);
 		node.setDefinicion(funciones.get(node.getNombre()));
 
 		if (node.getParametrosOpt() != null)
@@ -210,7 +214,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Return { Expr retorno; }
+	// class Return { Expr retorno; }
 	public Object visit(Return node, Object param) {
 
 		// super.visit(node, param);
@@ -221,27 +225,27 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class IntConstant { String valor; }
+	// class IntConstant { String valor; }
 	public Object visit(IntConstant node, Object param) {
 		return null;
 	}
 
-	//	class RealConstant { String valor; }
+	// class RealConstant { String valor; }
 	public Object visit(RealConstant node, Object param) {
 		return null;
 	}
 
-	//	class CharConstant { String valor; }
+	// class CharConstant { String valor; }
 	public Object visit(CharConstant node, Object param) {
 		return null;
 	}
 
-	//	class IdentConstant { String valor; }
+	// class IdentConstant { String valor; }
 	public Object visit(IdentConstant node, Object param) {
 		return null;
 	}
 
-	//	class Cast { Tipo tipo;  Expr valor; }
+	// class Cast { Tipo tipo; Expr valor; }
 	public Object visit(Cast node, Object param) {
 
 		// super.visit(node, param);
@@ -255,7 +259,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class ExprAritmetica { Expr left;  String operador;  Expr right; }
+	// class ExprAritmetica { Expr left; String operador; Expr right; }
 	public Object visit(ExprAritmetica node, Object param) {
 
 		// super.visit(node, param);
@@ -269,7 +273,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class ExprLogica { Expr left;  String operador;  Expr right; }
+	// class ExprLogica { Expr left; String operador; Expr right; }
 	public Object visit(ExprLogica node, Object param) {
 
 		// super.visit(node, param);
@@ -283,7 +287,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Not { Expr valor; }
+	// class Not { Expr valor; }
 	public Object visit(Not node, Object param) {
 
 		// super.visit(node, param);
@@ -294,7 +298,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class Array { Expr nombre;  Expr valor; }
+	// class Array { Expr nombre; Expr valor; }
 	public Object visit(Array node, Object param) {
 
 		// super.visit(node, param);
@@ -308,7 +312,7 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class AccesoStruct { Expr nombre;  String acceso; }
+	// class AccesoStruct { Expr nombre; String acceso; }
 	public Object visit(AccesoStruct node, Object param) {
 
 		// super.visit(node, param);
@@ -319,11 +323,11 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-	//	class LlamFuncExp { String nombre;  List<Expr> parametrosOpt; }
+	// class LlamFuncExp { String nombre; List<Expr> parametrosOpt; }
 	public Object visit(LlamFuncExp node, Object param) {
 
-		// super.visit(node, param);		
-		predicado(funciones.get(node.getNombre())!=null, "Funcion no definida", node);
+		// super.visit(node, param);
+		predicado(funciones.get(node.getNombre()) != null, "Funcion no definida: " + node.getNombre(), node);
 		node.setDefinicion(funciones.get(node.getNombre()));
 
 		if (node.getParametrosOpt() != null)
@@ -333,40 +337,45 @@ public class Identification extends DefaultVisitor {
 		return null;
 	}
 
-    /**
-     * predicado. Método auxiliar para implementar los predicados. Borrar si no se quiere usar.
-     *
-     * Ejemplos de uso (suponiendo que existe un método "esPrimitivo"):
-     *
-     *      1. predicado(esPrimitivo(expr.tipo), "La expresión debe ser de un tipo pimitivo", expr.getStart());
-     *      2. predicado(esPrimitivo(expr.tipo), "La expresión debe ser de un tipo pimitivo", expr);
-     *      3. predicado(esPrimitivo(expr.tipo), "La expresión debe ser de un tipo primitivo");
-     *
-     * NOTA: El método getStart() (ejemplo 1) indica la linea/columna del fichero fuente donde estaba el nodo
-     * (y así poder dar información más detallada de la posición del error). Si se usa VGen, dicho método
-     * habrá sido generado en todos los nodos del AST.
-     * No es obligatorio llamar a getStart() (ejemplo 2), ya que si se pasa el nodo, se usará por defecto dicha
-     * posición.
-     * Si no se quiere imprimir la posición del fichero, se puede omitir el tercer argumento (ejemplo 3).
-     *
-     * @param condicion     Debe cumplirse para que no se produzca un error
-     * @param mensajeError  Se imprime si no se cumple la condición
-     * @param posicionError Fila y columna del fichero donde se ha producido el error.
-     */
-    private void predicado(boolean condicion, String mensajeError, Position posicionError) {
-        if (!condicion)
-            errorManager.notify("Identificacion", mensajeError, posicionError);
-    }
+	/**
+	 * predicado. Método auxiliar para implementar los predicados. Borrar si no se
+	 * quiere usar.
+	 *
+	 * Ejemplos de uso (suponiendo que existe un método "esPrimitivo"):
+	 *
+	 * 1. predicado(esPrimitivo(expr.tipo), "La expresión debe ser de un tipo
+	 * pimitivo", expr.getStart()); 2. predicado(esPrimitivo(expr.tipo), "La
+	 * expresión debe ser de un tipo pimitivo", expr); 3.
+	 * predicado(esPrimitivo(expr.tipo), "La expresión debe ser de un tipo
+	 * primitivo");
+	 *
+	 * NOTA: El método getStart() (ejemplo 1) indica la linea/columna del fichero
+	 * fuente donde estaba el nodo (y así poder dar información más detallada de la
+	 * posición del error). Si se usa VGen, dicho método habrá sido generado en
+	 * todos los nodos del AST. No es obligatorio llamar a getStart() (ejemplo 2),
+	 * ya que si se pasa el nodo, se usará por defecto dicha posición. Si no se
+	 * quiere imprimir la posición del fichero, se puede omitir el tercer argumento
+	 * (ejemplo 3).
+	 *
+	 * @param condicion     Debe cumplirse para que no se produzca un error
+	 * @param mensajeError  Se imprime si no se cumple la condición
+	 * @param posicionError Fila y columna del fichero donde se ha producido el
+	 *                      error.
+	 */
+	private void predicado(boolean condicion, String mensajeError, Position posicionError) {
+		if (!condicion)
+			errorManager.notify("Identificacion", mensajeError, posicionError);
+	}
 
-    private void predicado(boolean condicion, String mensajeError, AST node) {
-        predicado(condicion, mensajeError, node.getStart());
-    }
+	private void predicado(boolean condicion, String mensajeError, AST node) {
+		predicado(condicion, mensajeError, node.getStart());
+	}
 
-    private void predicado(boolean condicion, String mensajeError) {
-        predicado(condicion, mensajeError, (Position) null);
-    }
+	private void predicado(boolean condicion, String mensajeError) {
+		predicado(condicion, mensajeError, (Position) null);
+	}
 
-
-    private ErrorManager errorManager;
-    private Map<String, DefFuncion> funciones = new HashMap<String, DefFuncion>();
+	private ErrorManager errorManager;
+	private Map<String, DefFuncion> funciones = new HashMap<String, DefFuncion>();
+	private Map<String, DefStruct> structs = new HashMap<String, DefStruct>();
 }
