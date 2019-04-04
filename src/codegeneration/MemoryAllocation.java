0e6473976129aafdae2bf100fd1b0a5511c79bed
martinp95 <martinpelaezdiaz@gmail.com>
2019-04-04 13:21:25 +0200

    Especificacion de codigo

--- src/codegeneration/MemoryAllocation.java
+++ src/codegeneration/MemoryAllocation.java
@@ -10,10 +10,7 @@ public class MemoryAllocation extends DefaultVisitor {
 
 	// class Programa { List<Definicion> definicion; }
 	public Object visit(Programa node, Object param) {
-
-		// super.visit(node, param);
 		int direccion = 0;
-
 		if (node.getDefinicion() != null)
 			for (Definicion child : node.getDefinicion()) {
 				if (child instanceof DefVariable) {
@@ -22,42 +19,32 @@ public class MemoryAllocation extends DefaultVisitor {
 				}
 				child.accept(this, param);
 			}
-
 		return null;
 	}
 
 	// class DefStruct { String nombre; List<Campo> campo; }
 	public Object visit(DefStruct node, Object param) {
-
-		// super.visit(node, param);
 		int direccion = 0;
-
 		if (node.getCampo() != null)
 			for (Campo child : node.getCampo()) {
 				child.setDireccion(direccion);
 				direccion += child.getTipo().getMemSize();
 				child.accept(this, param);
 			}
-
 		return null;
 	}
 
 	// class DefFuncion { String nombre; List<Parametro> parametro; Tipo tipo;
 	// List<DefVariable> defvariable; List<Sentencia> sentencia; }
 	public Object visit(DefFuncion node, Object param) {
-
-		// super.visit(node, param);
-
 		if (node.getParametro() != null) {
 			int direccion = 4;
-
 			for (int i = (node.getParametro().size() - 1); i >= 0; i--) {
 				node.getParametro().get(i).setDireccion(direccion);
 				direccion += node.getParametro().get(i).getTipo().getMemSize();
 				node.getParametro().get(i).accept(this, param);
 			}
 		}
-
 		if (node.getTipo() != null)
 			node.getTipo().accept(this, param);
 
@@ -69,12 +56,9 @@ public class MemoryAllocation extends DefaultVisitor {
 				child.accept(this, param);
 			}
 		}
-
 		if (node.getSentencia() != null)
 			for (Sentencia child : node.getSentencia())
 				child.accept(this, param);
-
 		return null;
 	}
-
 }
